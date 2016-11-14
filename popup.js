70ad37cbe661c26721388ca1a96ee122d38430c0
Marcelo Mazza <ux@marcelomazza.com.ar>
2016-11-14 00:04:18 -0300

    Use svg files for browser icons

--- popup.js
+++ popup.js
@@ -53,7 +53,7 @@ var canIUse = (function () {
         return;
       }
 
-      browserList.push('<li class="browser-icon ' + browser + '">' + browser + '</li>');
+      browserList.push('<li class="browser-icon ' + browser + '" title="' + browser + '">' + browser + '</li>');
 
       listVersions(browser, versions);
 
